7449a18307e2f1a1477aa38a6b45d5e2a4ee4e7a
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-01-29 04:58:22 +0000

    Add basic jquery post

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,17 @@
+/* global $ */
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+$.ajax({
+    url: 'https://edlnkr.herokuapp.com/auth/local',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      'username': 'test@test.com',
+      'password': 'test',
+    },
+    success: function(data) {
+      debugger
+      chrome.cookies.set(data);
+    }
+});
