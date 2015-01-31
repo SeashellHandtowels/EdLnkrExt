4345deab1f7c6c959339f374e7b0d4ee1d5ee514
Monsterblader <Monsterblader@gmail.com>
2015-01-31 11:46:01 -0800

    Able to select desired url and plan.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -4,14 +4,22 @@ angular.module('EdLnkr')
 
 .controller('MainController', ["$scope", "$http", function($scope, $http) {
   $scope.plans = [];
+  $scope.planId = "";
+  $scope.addLink = function() {
+    var selectedUrl;
+    chrome.tabs.getSelected(function(data) {
+      selectedUrl = data.url;
+      console.log(selectedUrl);
+    });
+  };
+
   $http.get('https://edlnkr.herokuapp.com/api/plans')
     .success(function(data) {
-      debugger;
       $scope.plans = data;
     })
     .error(function(err) {
-      debugger;
-      console.log("hello");
+      console.log("There was an error:");
+      console.log(err);
   });
 
 }]);
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,23 +1,20 @@
 /* global $ */
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
-$.ajax({
-  url: 'https://edlnkr.herokuapp.com/auth/local',
-  type: 'POST',
-  dataType: 'json',
-  data: {
-    'email': 'test@test.com',
-    'password': 'test'
-  },
-  success: function(data) {
-    chrome.cookies.set({"name":"token","url":"https://edlnkr.herokuapp.com","value": data.token},function (cookie){
-      console.log(JSON.stringify(cookie));
-      console.log(chrome.extension.lastError);
-      console.log(chrome.runtime.lastError);
-    });
-  },
-  error: function(err) {
-    console.log(arguments);
-  }
-});
+//$.ajax({
+//  url: 'https://edlnkr.herokuapp.com/auth/local',
+//  type: 'POST',
+//  dataType: 'json',
+//  data: {
+//    'email': 'test@test.com',
+//    'password': 'test'
+//  },
+//  success: function(data) {
+//    chrome.cookies.set({"name": "token2", "url": "https://edlnkr.herokuapp.com", "value": data.token}, function (cookie) {
+//      console.log(JSON.stringify(cookie));
+//    });
+//  },
+//  error: function(err) {
+//    console.log(arguments);
+//  }
+//});
