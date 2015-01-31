ec8d13a89e480b8843504f1640c69b9a0c95d38f
Monsterblader <Monsterblader@gmail.com>
2015-01-30 16:56:07 -0800

    Fix issue with $.ajax(). Implement sync storage of token.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,16 +2,21 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+chrome.storage.sync.get("token", function(data) {
+  console.log(data);
+});
 $.ajax({
-    url: 'https://edlnkr.herokuapp.com/auth/local',
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      'username': 'test@test.com',
-      'password': 'test',
-    },
-    success: function(data) {
-      debugger
-      chrome.cookies.set(data);
-    }
+  url: 'https://edlnkr.herokuapp.com/auth/local',
+  type: 'POST',
+  dataType: 'json',
+  data: {
+    'email': 'test@test.com',
+    'password': 'test',
+  },
+  success: function(data) {
+    chrome.storage.sync.set({token: data.token});
+  },
+  error: function(data) {
+    console.log(arguments);
+  }
 });
