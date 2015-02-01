f63cd2a16c539db2e26298bedd2c3eccfde91698
Monsterblader <Monsterblader@gmail.com>
2015-02-01 12:35:07 -0800

    Successfully sends object.  Need to refactor to send array or refactor server to recognize object.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -4,26 +4,23 @@ angular.module('EdLnkr')
 
 .controller('MainController', ["$scope", "$http", function($scope, $http) {
   $scope.plans = [];
-  $scope.planId = "";
+  $scope.chosenPlan = "";
   var url = "http://localhost:9000/api/plans";
   $scope.addLink = function(event) {
     event.preventDefault();
     chrome.tabs.getSelected(function(data) {
-      var newPlan = {};
-      newPlan.links = [{url: data.url, description: "hasdkl;"}, {url: "http://www.google.com", description: "asdf"}];
-      newPlan.title = ";khladgs";
-      newPlan._id = "54c976c39590686c18722a39";
-      $.ajax({
-        url: url + "/" + newPlan._id,
+      // TODO choose $.ajax or $http - determine which encoding is usable
+      $http({
+        url: url + "/" + $scope.chosenPlan._id,
         method: 'PATCH',
-        data: JSON.stringify(newPlan)
+        data: {url: data.url, description: $scope.linkDescription}
         //headers: {'Content-Type': 'application/json'}
       })
       .success(function(data) {
         console.log(data);
       })
       .error(function(data) {
-        console.log(arguments);
+        console.error(arguments);
       });
     });
   };
@@ -31,11 +28,10 @@ angular.module('EdLnkr')
   $http.get(url)
     .success(function(data) {
       $scope.plans = data;
-      console.log(data);
     })
     .error(function(err) {
       console.log("There was an error:");
-      console.log(err);
+      console.error(err);
   });
 
 }]);
