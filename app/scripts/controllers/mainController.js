c7bc7f9240a48ad3eadc6a963dedd0eb5fc70029
Monsterblader <Monsterblader@gmail.com>
2015-01-31 17:29:21 -0800

    Current state.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -7,14 +7,19 @@ angular.module('EdLnkr')
   $scope.planId = "";
   $scope.addLink = function() {
     chrome.tabs.getSelected(function(data) {
-      $scope.planId.links.push(data.url);
-      $http.put('https://edlnkr.herokuapp.com/api/plans/' + $scope.planId._id, $scope.planId)
-        .success(function(data) {
-          console.log(data);
-        })
-        .error(function(data) {
-          console.error(arguments);
-        });
+      $scope.planId.links.push({url: data.url});
+      //$http({
+      //  url: 'https://edlnkr.herokuapp.com/api/plans/' + $scope.planId._id,
+      //  method: 'PUT',
+      //  data: $scope.planId,
+      //  headers: {'Content-Type': 'application/json'}
+      //})
+      .success(function(data) {
+        console.log(data);
+      })
+      .error(function(data) {
+        console.error(arguments);
+      });
     });
   };
 
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,20 +1,20 @@
 /* global $ */
 'use strict';
-
-$.ajax({
-  url: 'https://edlnkr.herokuapp.com/auth/local',
-  type: 'POST',
-  dataType: 'json',
-  data: {
-    'email': 'test@test.com',
-    'password': 'test'
-  },
-  success: function(data) {
-    chrome.cookies.set({"name": "token2", "url": "https://edlnkr.herokuapp.com", "value": data.token}, function (cookie) {
-      console.log(JSON.stringify(cookie));
-    });
-  },
-  error: function(err) {
-    console.error(arguments);
-  }
-});
+//
+//$.ajax({
+//  url: 'https://edlnkr.herokuapp.com/auth/local',
+//  type: 'POST',
+//  dataType: 'json',
+//  data: {
+//    'email': 'test@test.com',
+//    'password': 'test'
+//  },
+//  success: function(data) {
+//    chrome.cookies.set({"name": "token2", "url": "https://edlnkr.herokuapp.com", "value": data.token}, function (cookie) {
+//      console.log(JSON.stringify(cookie));
+//    });
+//  },
+//  error: function(err) {
+//    console.error(arguments);
+//  }
+//});
