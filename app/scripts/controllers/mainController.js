4c6675e7902ffdcae3e5753851b2d17337bcdbc5
Monsterblader <Monsterblader@gmail.com>
2015-02-01 21:31:11 -0800

    Edit readme.md, add alert on successful save of link, remove unneeded permission from maifest.js, comment code.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -5,8 +5,10 @@ angular.module('EdLnkr')
 .controller('MainController', ['$scope', '$http', function($scope, $http) {
   $scope.plans = [];
   $scope.chosenPlan = '';
+  // To test this extension with a local instance of EdLnkr, use the following url.
   //var url = 'http://localhost:9000/api/plans';
   var url = 'https://edlnkr.herokuapp.com/api/plans';
+
   $scope.addLink = function(event) {
     event.preventDefault();
     chrome.tabs.getSelected(function(data) {
@@ -18,7 +20,7 @@ angular.module('EdLnkr')
         data: {links: result}
       })
       .success(function(data) {
-        console.log(data);
+        alert('Link saved');
       })
       .error(function(data) {
         console.error(data);
