74d3dd93244ad26352689d4c0eb40f1b92dd30ee
Monsterblader <Monsterblader@gmail.com>
2015-02-02 12:26:34 -0800

    Extension displays notifications upon success or failure of adding a link.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -6,8 +6,8 @@ angular.module('EdLnkr')
   $scope.plans = [];
   $scope.chosenPlan = '';
   // To test this extension with a local instance of EdLnkr, use the following url.
-  //var url = 'http://localhost:9000/api/plans';
-  var url = 'https://edlnkr.herokuapp.com/api/plans';
+  var url = 'http://localhost:9000/api/plans';
+  // var url = 'https://edlnkr.herokuapp.com/api/plans';
   $scope.addLink = function(event) {
     event.preventDefault();
     chrome.tabs.getSelected(function(data) {
@@ -18,10 +18,16 @@ angular.module('EdLnkr')
         data: $scope.chosenPlan
       })
       .success(function(data) {
+        chrome.notifications.create("msg", {type: "basic", title: "title", message: "Link successfully added.", iconUrl: "images/icon-38.png"}, function() {
+          console.log(arguments);
+        });
         window.close();
       })
       .error(function(data) {
         console.error(data);
+        chrome.notifications.create("err", {type: "basic", title: "title", message: "Sorry.  There was an error.", iconUrl: "images/icon-38.png"}, function() {
+          console.log(arguments);
+        });
         window.close();
       });
     });
